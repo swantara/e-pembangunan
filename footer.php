509ef5bb25d62c5ee7f52f3b570c50035cc7cb42
r4ve <nyoman.swan@gmail.com>
2017-11-26 06:20:13 +0800

    commit 2

---
+++ footer.php
@@ -0,0 +1,10 @@
+  <!-- Footer -->
+  <div class="footer text-muted">
+    &copy; 2015. <a href="#">Limitless Web App Kit</a> by <a href="http://themeforest.net/user/Kopyov" target="_blank">Eugene Kopyov</a>
+  </div>
+  <!-- /footer -->
+
+</body>
+
+<!-- Mirrored from demo.interface.club/limitless/layout_4/LTR/default/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 25 Nov 2017 08:37:26 GMT -->
+</html>
---
+++ header.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from demo.interface.club/limitless/layout_4/LTR/default/index.php by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 25 Nov 2017 08:26:18 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Limitless - Responsive Web Application Kit by Eugene Kopyov</title>
+
+  <!-- Global stylesheets -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
+  <link href="assets/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
+  <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">
+  <link href="assets/css/core.css" rel="stylesheet" type="text/css">
+  <link href="assets/css/components.css" rel="stylesheet" type="text/css">
+  <link href="assets/css/colors.css" rel="stylesheet" type="text/css">
+  <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+  <!-- /global stylesheets -->
+
+  <!-- Core JS files -->
+  <script type="text/javascript" src="assets/js/plugins/loaders/pace.min.js"></script>
+  <script type="text/javascript" src="assets/js/core/libraries/jquery.min.js"></script>
+  <script type="text/javascript" src="assets/js/core/libraries/bootstrap.min.js"></script>
+  <script type="text/javascript" src="assets/js/plugins/loaders/blockui.min.js"></script>
+  <script type="text/javascript" src="assets/js/plugins/ui/nicescroll.min.js"></script>
+  <script type="text/javascript" src="assets/js/plugins/ui/drilldown.js"></script>
+  <!-- /core JS files -->
+
+  <!-- Theme JS files -->
+  <script type="text/javascript" src="assets/js/plugins/visualization/d3/d3.min.js"></script>
+  <script type="text/javascript" src="assets/js/plugins/visualization/d3/d3_tooltip.js"></script>
+  <script type="text/javascript" src="assets/js/plugins/forms/styling/switchery.min.js"></script>
+  <script type="text/javascript" src="assets/js/plugins/forms/styling/uniform.min.js"></script>
+  <script type="text/javascript" src="assets/js/plugins/forms/selects/bootstrap_multiselect.js"></script>
+  <script type="text/javascript" src="assets/js/plugins/ui/moment/moment.min.js"></script>
+  <script type="text/javascript" src="assets/js/plugins/pickers/daterangepicker.js"></script>
+
+  <script type="text/javascript" src="assets/js/core/app.js"></script>
+  <script type="text/javascript" src="assets/js/pages/dashboard.js"></script>
+  <!-- /theme JS files -->
+
+</head>
+
+<body>
+
+  <!-- Main navbar -->
+  <div class="navbar navbar-default navbar-fixed-top">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php"><img src="assets/images/logo.png" alt=""></a>
+    </div>
+    <ul class="nav navbar-nav no-border visible-xs-block">
+      <li><a class="text-center collapsed" data-toggle="collapse" data-target="#navbar-second-toggle"><i class="icon-menu7"></i></a></li>
+    </ul>
+
+    <div class="navbar-collapse collapse" id="navbar-second-toggle">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="index.php"><i class="icon-home2 position-left"></i> Beranda</a></li>
+        <li><a href="index.php"><i class="icon-design position-left"></i> Perencanaan</a></li>
+        <li><a href="index.php"><i class="icon-versions position-left"></i> Lelang</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            <i class="icon-statistics position-left"></i> Realisasi <span class="caret"></span>
+          </a>
+          <ul class="dropdown-menu width-200">
+            <li><a href="starters/layout_boxed.html"><i class="icon-stats-growth"></i> Realisasi Keuangan</a></li>
+            <li><a href="starters/layout_sidebar_sticky.html"><i class="icon-clipboard2"></i> Realisasi Fisik</a></li>
+          </ul>
+        </li>
+        <li class="dropdown dropdown-user">
+          <a style="padding-top: 18px;" class="dropdown-toggle" data-toggle="dropdown">
+            <img src="assets/images/demo/users/guest-red.png" alt="">
+            <span>Guest</span>
+            <i class="caret"></i>
+          </a>
+          <ul class="dropdown-menu dropdown-menu-right">
+            <li><a href="#"><i class="icon-user"></i> Login</a></li>
+            <!-- <li><a href="#"><i class="icon-user-plus"></i> My profile</a></li>
+            <li><a href="#"><i class="icon-coins"></i> My balance</a></li>
+            <li><a href="#"><span class="badge badge-warning pull-right">58</span> <i class="icon-comment-discussion"></i> Messages</a></li>
+            <li class="divider"></li>
+            <li><a href="#"><i class="icon-cog5"></i> Account settings</a></li>
+            <li><a href="#"><i class="icon-switch2"></i> Logout</a></li> -->
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <!-- /second navbar -->
+
